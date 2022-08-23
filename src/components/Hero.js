e7a428a3bb7e466869612465a072e52e01e9cc5a
andrey <andreytorrente@gmail.com>
2022-08-23 12:04:47 -0600

    fixed scrollTo navbar overlapping

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,25 +19,25 @@ export const Hero = () => {
 };
 
 document.addEventListener("click", (e) => {
-  if (e.target.matches(".arrows-down") || e.target.matches(".link-projects")) {
-    e.preventDefault();
-    const projectSection = document.querySelector(".projects").offsetTop - document.querySelector(".nav").offsetHeight;
-    window.scrollTo(0, projectSection);
-  }
+  // if (e.target.matches(".arrows-down") || e.target.matches(".link-projects")) {
+  //   e.preventDefault();
+  //   const projectSection = document.querySelector(".projects").offsetTop - document.querySelector(".nav").offsetHeight;
+  //   window.scrollTo(0, projectSection);
+  // }
   
-  if (e.target.matches(".link-about")) {
-    e.preventDefault();
-    const aboutSection = document.querySelector(".about").offsetTop - document.querySelector(".nav").offsetHeight;
-    window.scrollTo(0, aboutSection);
-  }
+  // if (e.target.matches(".link-about")) {
+  //   e.preventDefault();
+  //   const aboutSection = document.querySelector(".about").offsetTop - document.querySelector(".nav").offsetHeight;
+  //   window.scrollTo(0, aboutSection);
+  // }
   
-  if (e.target.matches(".link-contact")) {
-    e.preventDefault();
-    const contactSection = document.querySelector(".contact").offsetTop - document.querySelector(".nav").offsetHeight;
-    window.scrollTo(0, contactSection);
-  }
-  if (e.target.matches(".link-home") || e.target.matches(".nav__logo a")) {
-    e.preventDefault();
-    window.scrollTo(0, document.querySelector(".hero"));
-  }
+  // if (e.target.matches(".link-contact")) {
+  //   e.preventDefault();
+  //   const contactSection = document.querySelector(".contact").offsetTop - document.querySelector(".nav").offsetHeight;
+  //   window.scrollTo(0, contactSection);
+  // }
+  // if (e.target.matches(".link-home") || e.target.matches(".nav__logo a")) {
+  //   e.preventDefault();
+  //   window.scrollTo(0, document.querySelector(".hero"));
+  // }
 });
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,20 +22,42 @@ export const Navbar = () => {
 document.addEventListener("click", (e) => {
   const links = document.querySelector(".nav__links");
 
-  if (e.target.matches(".nav__icon")) {
+  // if (e.target.matches(".nav__icon")) {
 
     
-    if(links.classList.contains("links-active")){
-      document.querySelector(".overlay").remove();
-      e.target.classList.add("bi-list-nested")
-      e.target.classList.remove("bi-x-lg")
-    }else{
-      document.getElementById("app").insertAdjacentHTML("beforeend", "<div class='overlay'></div>");
-      e.target.classList.remove("bi-list-nested")
-      e.target.classList.add("bi-x-lg")
-    }
-    links.classList.toggle("links-active");
+  //   if(links.classList.contains("links-active")){
+  //     document.querySelector(".overlay").remove();
+  //     e.target.classList.add("bi-list-nested")
+  //     e.target.classList.remove("bi-x-lg")
+  //   }else{
+  //     document.getElementById("app").insertAdjacentHTML("beforeend", "<div class='overlay'></div>");
+  //     e.target.classList.remove("bi-list-nested")
+  //     e.target.classList.add("bi-x-lg")
+  //   }
+  //   links.classList.toggle("links-active");
 
+  // }
+
+  if (e.target.matches(".arrows-down") || e.target.matches(".link-projects")) {
+    e.preventDefault();
+    const projectSection = document.querySelector(".projects").offsetTop - document.querySelector(".nav").offsetHeight+30;
+    window.scrollTo(0, projectSection);
+  }
+  
+  if (e.target.matches(".link-about")) {
+    e.preventDefault();
+    const aboutSection = document.querySelector(".about").offsetTop - document.querySelector(".nav").offsetHeight+40;
+    window.scrollTo(0, aboutSection);
+  }
+  
+  if (e.target.matches(".link-contact")) {
+    e.preventDefault();
+    const contactSection = document.querySelector(".contact").offsetTop - document.querySelector(".nav").offsetHeight+30;
+    window.scrollTo(0, contactSection);
+  }
+  if (e.target.matches(".link-home") || e.target.matches(".nav__logo a")) {
+    e.preventDefault();
+    window.scrollTo(0, document.querySelector(".hero"));
   }
   
   // if (e.target.matches(".nav__links li a") || e.target.matches(".overlay") ) {
