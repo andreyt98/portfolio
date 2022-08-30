ed4521bc3a942c8f56f87cceee731ca41d6568ca
andrey <andreytorrente@gmail.com>
2022-08-29 20:56:20 -0600

    change hero background, header style and add social links

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,43 +1,22 @@
-import { avatar, arrows } from "../../helpers/icons.js";
-
+import { arrows,socials } from "../../helpers/icons.js";
+ const{linkedin,github} = socials;
 export const Hero = () => {
   const hero = document.createElement("DIV");
   hero.classList.add("hero");
 
   hero.innerHTML = `
-    <img class="pattern-bg" src="https://res.cloudinary.com/andreyt/image/upload/q_auto/v1660263699/Portfolio-assets/pattern_qtvc2x.webp" alt="hero-pattern">
+    <div class="blur-bg"> </div>
     <h1>
         <span>Andrey Torrente</br></span>
         <span class=bolder >Frontend developer</span>
     </h1>
 
-    
-    <div class="avatar">${avatar}</div>
-    ${arrows} 
+    <div class="socials">
+          <a href="https://www.linkedin.com/in/andrey-torrente" target="_blank">${linkedin}</a>
+          <a href="https://www.github.com/andreyt98" target="_blank">${github}</a>
+        </div>
+
+        ${arrows} 
     `;
   return hero;
 };
-
-document.addEventListener("click", (e) => {
-  // if (e.target.matches(".arrows-down") || e.target.matches(".link-projects")) {
-  //   e.preventDefault();
-  //   const projectSection = document.querySelector(".projects").offsetTop - document.querySelector(".nav").offsetHeight;
-  //   window.scrollTo(0, projectSection);
-  // }
-  
-  // if (e.target.matches(".link-about")) {
-  //   e.preventDefault();
-  //   const aboutSection = document.querySelector(".about").offsetTop - document.querySelector(".nav").offsetHeight;
-  //   window.scrollTo(0, aboutSection);
-  // }
-  
-  // if (e.target.matches(".link-contact")) {
-  //   e.preventDefault();
-  //   const contactSection = document.querySelector(".contact").offsetTop - document.querySelector(".nav").offsetHeight;
-  //   window.scrollTo(0, contactSection);
-  // }
-  // if (e.target.matches(".link-home") || e.target.matches(".nav__logo a")) {
-  //   e.preventDefault();
-  //   window.scrollTo(0, document.querySelector(".hero"));
-  // }
-});
