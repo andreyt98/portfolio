d3439cac31520455a2bf9ce42ceda5e0b3359712
Andrey <devandrey98@gmail.com>
2023-10-28 13:47:20 -0600

    feat: changed container and contact form border

--- helpers/resetForm.js
+++ helpers/resetForm.js
@@ -3,8 +3,8 @@ export const resetForm = (name, message,email) =>{
     name.value = "";
     message.value = "";
     email.value = "";
-    name.style.border = "1px solid #d2bdfc";
-    message.style.border = "1px solid #d2bdfc";
-    email.style.border = "1px solid #d2bdfc";
+    name.style.border = "1px solid #8b8b8bd0";
+    message.style.border = "1px solid #8b8b8bd0";
+    email.style.border = "1px solid #8b8b8bd0";
     document.querySelector("#form-error").style.display = "none";
 }
